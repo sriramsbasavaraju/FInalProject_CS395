b68322fd5f09f9155c7a509aceb078cbbbc0904d
Rohan Mengle <69176595+Megz-Mugz@users.noreply.github.com>
2024-02-25 13:12:08 +0000

    rendered home page using ejs (refactored a lot)

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello Rohan');
-});
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.render("home")
+})
 
 app.listen(5000, () => {
   console.log('Server is running on http://localhost:5000');
