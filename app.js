024dd719d0851725e2b3a12da704156218a8e6e2
Rohan Mengle <69176595+Megz-Mugz@users.noreply.github.com>
2024-02-25 14:04:23 +0000

    linked css to project

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
 app.set('view engine', 'ejs');
 
+
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(req, res){
   res.render("home")
